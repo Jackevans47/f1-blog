cce549ec206b04dfe38a99c7ae8cbe1a095c2591
jackevans47 <jack_evans47@outlook.com>
2024-10-07 18:49:58 +0100

    add deployment settings

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL",
+    "postgres://uhr9fwdzuy6:Y4SXhgxqrWWm@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/park_stuck_cover_221267",
+)
