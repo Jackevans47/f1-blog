1e29fd3487c9fcb7ffbf71fd6e053b704c2cf8b9
jackevans47 <jack_evans47@outlook.com>
2024-10-08 17:11:15 +0100

    add secret key

--- env.py
+++ env.py
@@ -2,5 +2,8 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL",
-    "postgres://uhr9fwdzuy6:Y4SXhgxqrWWm@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/park_stuck_cover_221267",
+    "<postgres://uhr9fwdzuy6:Y4SXhgxqrWWm@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/park_stuck_cover_221267",
+)
+os.environ.setdefault(
+    "SECRET_KEY", "i4@ihguc9=*u(kw^y$0ghr8mzxqc=ag-0komv%cso5$09x5hr)"
 )
--- f1/settings.py
+++ f1/settings.py
@@ -25,9 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-gye$tl#g3k_d==(+^co27+wu%@8)$swj8ztigx#1qvpm##8lno"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
