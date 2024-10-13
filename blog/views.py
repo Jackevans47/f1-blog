aea30107d7ace50969a3a19be3e1ebefcb504676
jackevans47 <jack_evans47@outlook.com>
2024-10-13 11:41:10 +0100

    add comment display and comment form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -12,23 +13,23 @@ class PostList(generic.ListView):
 
 def post_detail(request, slug):
     """
-    Display an individual :model:`blog.Post`.
-
-    **Context**
-
-    ``post``
-        An instance of :model:`blog.Post`.
-
-    **Template:**
-
-    :template:`blog/post_detail.html`
+    Display an individual model
     """
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+    comment_form = CommentForm()
 
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post, "coder": "Jack Evans"},
+        {
+            "post": post,
+            "coder": "Jack Evans",
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+        },
     )
